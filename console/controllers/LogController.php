0f4d9882d1896eac59cf3d42975534c8fe9c0887
liziying <876505905@qq.com>
2018-06-20 16:57:28 +0800

    日志处理

--- console/controllers/LogController.php
+++ console/controllers/LogController.php
@@ -2,271 +2,89 @@
 /**
  * Created by PhpStorm.
  * User: lychee
- * Date: 2018/4/11
- * Time: 16:35
+ * Date: 2018/6/20
+ * Time: 15:58
  */
 
 namespace console\controllers;
 
 use Yii;
-use backend\components\MySSH;
-use backend\models\AppLog;
-use backend\models\Mac;
-use backend\models\ProgramLog;
-use backend\models\TimelineLog;
 use yii\console\Controller;
-use yii\console\ExitCode;
 
 /**
- * 日志处理类
- * Class paradeTimer
- * @package Applications\YourApp\timer
+ * 日志分析控制器
+ * Class LogController
+ * @package console\controllers
  */
 class LogController extends Controller
 {
-    protected $path = '/var/www/log/app/';
-    protected $yesterdayLog;
+    public $db = 14;
 
-    public function actionIndex()
+    //日志分析
+    public function actionAnalyse()
     {
-        $this->yesterdayLog = $this->getYesterdayLog();
+        //读取redis
+        $redis = Yii::$app->redis;
+        $redis->select($this->db);
 
-        if (file_exists($this->yesterdayLog)) {
-            $start = memory_get_usage(true);
-            $this->analyseUserBehavior($this->yesterdayLog);
-            $this->analyseInterface($this->yesterdayLog);
-            $this->analyseProgram($this->yesterdayLog);
-            $this->getSSH()->close();
-            $end = memory_get_usage(true);
-            $usage = $end - $start;
-            echo "内存消耗 {$usage}\n";
+        //每次获取长度
+        $length = $redis->llen('log');
+
+        while ($length) {
+            $this->deal($redis);
+            $length--;
         }
-        return ExitCode::OK;
     }
-
-
-
-    public function analyseProgram($yesterdayLog)
-    {
-        //统计本地解析 前二十个节目播放数据
-        $shell = "cat $yesterdayLog | grep local | awk -F '|' '{print $9}' | sort -nr | uniq -c | sort -k1 -nr";
-        $all = $this->getSSH()->exec($shell);
-        $all = array_filter(preg_split('/[\r\n]+/s', $all));
-
-        $local_statics = [];
-
-        if (!empty($all)) {
-            foreach ($all as $k => $item) {
-                if ($k > 20) {
-                    continue;
-                }
-                $item = explode(' ', trim($item));
-                $name = $item[1];
-                $num = $item[0];
-                $local_statics[$name] = $num;
-            }
-        }
-
-        //统计服务器端
-        $shell = "cat $yesterdayLog | grep -E 'tvnet|viettel|sohatv|thvl|hoabinhtv|v4live|migu|sohulive|hplus|newmigu|haoqu|tencent|vtv|ott' | awk -F '|' '{print $9}' | sort -nr | uniq -c | sort -k1 -nr";
-        $all = $this->getSSH()->exec($shell);
-        $all = array_filter(preg_split('/[\r\n]+/s', $all));
-
-        $server_statics = [];
-        if (!empty($all)) {
-            foreach ($all as $k => $item) {
-                if ($k > 20) {
-                    continue;
-                }
-                $item = explode(' ', trim($item));
-                $name = $item[1];
-                $num = $item[0];
-                $server_statics[$name] = trim($num);
-            }
-        }
-
-        //统计全部
-        $shell = "cat $yesterdayLog | grep -E 'tvnet|viettel|sohatv|thvl|hoabinhtv|v4live|migu|sohulive|hplus|newmigu|haoqu|tencent|vtv|ott|local' | awk -F '|' '{print $9}' | sort -nr | uniq -c | sort -k1 -nr";
-        $all = $this->getSSH()->exec($shell);
-        $all = array_filter(preg_split('/[\r\n]+/s', $all));
-
-        $all_statics = [];
-        if (!empty($all)) {
-            foreach ($all as $k => $item) {
-                $item = explode(' ', trim($item));
-                $name = $item[1];
-                $num = $item[0];
-                $all_statics[$name] = trim($num);
-            }
-        }
-
-        //统计全部节目数字
-        $shell = "cat $yesterdayLog | grep -E 'tvnet|viettel|sohatv|thvl|hoabinhtv|v4live|migu|sohulive|hplus|newmigu|haoqu|tencent|vtv|ott|local' | wc -l";
-        $sum = $this->getSSH()->exec($shell);
-
-        $programLog = new ProgramLog();
-        $programLog->local_program = json_encode($local_statics);
-        $programLog->server_program = json_encode($server_statics);
-        $programLog->all_program = json_encode($all_statics);
-        $programLog->all_program_sum = trim($sum);
-        $programLog->date = date('Y-m-d', $this->_setDate());
-        $programLog->save(false);
-
-    }
-
 
     /**
-     * 按时间维度 查看日志
-     * @param $yesterdayLog
+     * 处理日志统计
+     * @param $redis yii\redis\Connection
      */
-    public function analyseInterface($yesterdayLog)
+    public function deal($redis)
     {
+        $log = $redis->lpop('log');
 
-        $time = $this->getSSH()->exec("cat $yesterdayLog | awk -F '|' '{print $3 }' | awk -F ':' '{print $1}' | sort -u");
-        $times = array_filter(preg_split('/[;\r\n]+/s', $time));
+        if ($log) {
+            $log = explode('|', $log);
+            list($time, $ip, $json, $error) = $log;
+            $data = json_decode($json, true);
+            $uid = $data['uid'];
+            $header = $data['header'];
+            $requestData = $data['data'];
 
-        $totalRequest = [];
-        $watchRequest = [];
-        $tokenRequest = [];
-        $localRequest = [];
-        $serverRequest = [];
+            // 按用户进行统计
+            $isExist = (boolean)$redis->exists($uid);
+            $key = date('m/d:') . $uid;
 
-        for ($i = 0; $i <= 23; $i++) {
-            if (in_array($i, $times)) {
-                $time = sprintf("%02d", $i);
-                $shell = "cat $yesterdayLog | grep 'time|{$time}' | wc -l";
-                $totalRequest[$i] = trim($this->getSSH()->exec($shell));
-
-                $watch_shell = "cat $yesterdayLog | grep 'time|{$time}' | grep -E 'tvnet|viettel|sohatv|thvl|hoabinhtv|v4live|migu|sohulive|hplus|newmigu|haoqu|tencent|vtv|ott|local' | wc -l";
-                $watch = $this->getSSH()->exec($watch_shell);
-                $watchRequest[] = trim($watch);
-
-                $local_shell = "cat $yesterdayLog | grep 'time|{$time}' | grep -E 'local' | wc -l";
-                $local = $this->getSSH()->exec($local_shell);
-                //本地解析
-                $localRequest[] = trim($local);
-
-                //服务器解析
-                $serverRequest[] = end($watchRequest) - end($localRequest);
-
-                $token_shell = "cat $yesterdayLog | grep 'time|{$time}' | grep 'getClientToken' | wc -l";
-                $watch = $this->getSSH()->exec($token_shell);
-                $tokenRequest[] = trim($watch);
-
+            if ($isExist === false) {
+                $redis->hmset($key, $header, 1);
+                $redis->expire($key, 86400 * 2);
             } else {
-                $localRequest[] = $serverRequest[] = $tokenRequest[] = $watchRequest[] = $totalRequest[] = 0;
+                $redis->hincrby($key, $header, 1);
             }
-        }
 
-        $timeLineLog = new TimelineLog();
-        $timeLineLog->year = date('Y');
-        $timeLineLog->total_line = json_encode($totalRequest);
-        $timeLineLog->watch_line = json_encode($watchRequest);
-        $timeLineLog->token_line = json_encode($tokenRequest);
-        $timeLineLog->year = json_encode($localRequest);
-        $timeLineLog->local_watch_line = json_encode($localRequest);
-        $timeLineLog->server_watch_line = json_encode($serverRequest);
-        $timeLineLog->date = date('Y-m-d', $this->_setDate());
-        if (!TimelineLog::findOne(['date' => date('Y-m-d', $this->_setDate())])) {
-            $timeLineLog->save(false);
-        }
-
-    }
-
-    /**
-     * 处理昨天产生的日志
-     * @param $yesterdayLog
-     */
-    public function analyseUserBehavior($yesterdayLog)
-    {
-        $data = $this->getSSH()->exec("cat $yesterdayLog | grep Token | awk -F '|' '{print $9 }' | sort -u");
-        $activeUser = array_filter(preg_split('/[;\r\n]+/s', $data));
-        echo "需要处理" . count($activeUser) . PHP_EOL;
-        if (!empty($activeUser)) {
-            foreach ($activeUser as $user) {
-
-                //对每个用户进行执行统计
-                $log = $this->_initialData();
-                $log['mac'] = $user;
-                $log['login_time'] = $this->_grep($user, "awk -F '|' '{print $3}' | head -n 1");
-                $log['last_time'] = $this->_grep($user, "awk -F '|' '{print $3}' | tail -n 1");
-                $log['total_request'] = $this->_grep($user, "awk -F '|' '{print $3}'| wc -l");
-                $log['token_request'] = $this->_grep($user, "grep 'getClientToken' | wc -l");
-                $log['token_success'] = $this->_grep($user, "grep 'getClientToken' | grep 'error|success' | wc -l");
-                $log['ott_request'] = $this->_grep($user, "grep 'getOttNewList'  | wc -l");
-                $log['renew_request'] = $this->_grep($user, "grep 'renew'  | wc -l");
-                $log['parade_request'] = $this->_grep($user, "grep 'getParadeList'  | wc -l");
-                $log['time_request'] = $this->_grep($user, "grep 'getServerTime' | wc -l");
-                $log['iptv_request'] = $this->_grep($user, "grep 'getIptvList'  | wc -l");
-                $log['app_request'] = $this->_grep($user, "grep 'getApp|getNewApp' | wc -l");
-                $log['auth_request'] = $this->_grep($user, "grep 'authMacDevice' | wc -l");
-                $log['register_request'] = $this->_grep($user, "grep 'register' | wc -l");
-                $log['market_request'] = $this->_grep($user, "grep 'getAppMarket' | wc -l");
-
-                $log['firmware_request'] = $this->_grep($user, "grep 'getFirmware|getAndroidFirmware' | wc -l ");
-                $log['ip_change'] = $this->_grep($user, "awk -F '|' '{print $5}' | sort -u | wc -l") - 1;
-
-                if ($log['last_time'] < $log['login_time']) {
-                    $lastTime = strtotime($log['last_time']) + 86400;
-                } else {
-                    $lastTime = strtotime($log['last_time']);
-                }
-
-                $during = ($lastTime - strtotime($log['login_time']));
-                if ($during == 0) {
-                    continue;
-                }
-
-                $log['request_rate'] = ceil($log['total_request'] / $during);
-                $log['active_hour'] = sprintf("%2.2f", $during / 3600);
-                $log['is_valid'] = !is_null(Mac::find()->where(['mac' => $log['mac']])->one()) ? 1 : 0;
-                $log['exception'] = 0;
-                //$log['request_tv'] = str_replace('\r','|', trim($this->getSSH()->exec("cat $yesterdayLog | grep $user | grep -vE 'getChannelIcon|getParade|default|getServerTime'| awk -F '|' '{print $11}' || sort -r -u")));
-
-                $appLog = new AppLog();
-                foreach ($log as $field => $value) {
-                    $appLog->$field = $value;
-                }
-                $appLog->save(false);
-                echo "正在处理{$user}...请求总次数{$log['total_request']}\n";
+            // 按小时进行统计
+            $key = date('m/d:H');
+            $isExist = (boolean)$redis->exists($key);
+            if ($isExist === false) {
+                $redis->hmset($key, $header, 1);
+                $redis->expire($key, 86400 * 2);
+            } else {
+                $redis->hincrby($key, $header, 1);
             }
+
+            // 按天数进行统计
+            $key = date('m/d');
+            $isExist = (boolean)$redis->exists($key);
+            if ($isExist === false) {
+                $redis->hmset($key, $header, 1);
+                $redis->expire($key, 86400 * 2);
+            } else {
+                $redis->hincrby($key, $header, 1);
+            }
+
         }
-
     }
 
-    protected function getSSH()
-    {
-        return MySSH::singleton();
-    }
-
-    protected function getYesterdayLog()
-    {
-        return $this->path . '/'. date('Y', $this->_setDate()) . '/' . sprintf('%02d', date('m', $this->_setDate())) . '/' . date('Ymd', $this->_setDate()) . '.log';
-    }
-
-    protected function _grep($user, $condition)
-    {
-        $shell = "cat {$this->yesterdayLog} | grep '|{$user}|' | $condition";
-        return trim($this->getSSH()->exec($shell));
-    }
-
-    /**
-     *
-     */
-    protected function _initialData()
-    {
-        $strTime = date('Y-m-d');
-
-        return [
-            'month' => date('m'),
-            'week' => ceil(((strtotime($strTime) - strtotime(date('Y') . "-01-01 00:00:00"))) / (7 * 86400)),
-            'date' => date('Y-m-d', $this->_setDate())
-        ];
-    }
-
-    private function _setDate()
-    {
-        return strtotime('yesterday');
-    }
 
 }
