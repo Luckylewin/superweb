11c9d17ddca6a95180639ca989aca686a74f3a2e
liziying <876505905@qq.com>
2018-08-24 13:16:04 +0800

    修改迁移bug

--- console/migrations/m180824_034106_add_list_flag_to_main_class_table.php
+++ console/migrations/m180824_034106_add_list_flag_to_main_class_table.php
@@ -32,7 +32,7 @@ class m180824_034106_add_list_flag_to_main_class_table extends Migration
         // 添加一个字段用于展示名称
         $this->addColumn('ott_main_class', 'list_name', 'varchar(50) not null');
         try {
-            Yii::$app->db->createCommand('update ott_main_class set list_name=name')->execute()
+            Yii::$app->db->createCommand('update ott_main_class set list_name=name')->execute();
             } catch (\Exception $e) {
 
         }
