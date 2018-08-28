850ce1eeb0fd9c4edfd3620d73ec7c434da36061
liziying <876505905@qq.com>
2018-08-28 18:28:22 +0800

    修改预告

--- console/models/parade/vn.php
+++ console/models/parade/vn.php
@@ -10,16 +10,22 @@ namespace console\models\parade;
 
 use console\components\MySnnopy;
 use Symfony\Component\DomCrawler\Crawler;
+use yii\helpers\ArrayHelper;
 
 
 class vn extends CommonParade
 {
+
+    public $tomorrow;
+
     /*
         *节目预告
     */
     public function start()
     {
-        $this->getviebao();//viebao
+        $this->getviebao();
+        $this->getHtvOne();
+        //$this->getHtvTwo();
     }
 
     //vietbao
@@ -48,7 +54,6 @@ class vn extends CommonParade
         ];
 
         foreach ($data as $key => $url) {
-            //echo "get the parade of $key" ,PHP_EOL;
             $this->crawlViebao($key, $url);
             $this->_sleep(2, 4);
         }
@@ -83,8 +88,10 @@ class vn extends CommonParade
 
         $paradeData = [];
         $crawler->filter('div.chanel-detail')->last()->filter('div.row')->each(function(Crawler $node,$i) use (&$paradeData) {
-            $paradeData[]['parade_time'] = $node->filter('div.time-tv')->eq(0)->text();
-            $paradeData[]['parade_name'] = $node->filter('div.t-detail a span')->eq(0)->text();
+            $item = [];
+            $item['parade_time'] = $node->filter('div.time-tv')->eq(0)->text();
+            $item['parade_name'] = $node->filter('div.t-detail a span')->eq(0)->text();
+            $paradeData[] = $item;
         });
 
         //查找频道是否存在
@@ -93,18 +100,158 @@ class vn extends CommonParade
         return true;
     }
 
-    //htvplus
-    public function getChannelParade()
+
+    public function getHtvTwo()
     {
-        $data = [
-            'htv3' => 'http://hplus.com.vn/xem-kenh-htv3-2535.html',
-            'htv7' => 'http://hplus.com.vn/xem-kenh-htv7-sd-10036.html',
-            'htv9' => 'http://hplus.com.vn/xem-kenh-htv9-sd-10037.html',
-            'THUAN VIET' => 'http://hplus.com.vn/xem-kenh-htvc-thuan-viet-2396',
-            'htv4' => 'http://hplus.com.vn/xem-kenh-htv4-2528.html',
-            'htv2' => 'http://hplus.com.vn/xem-kenh-htv2-hd-2669.html'
+        // 获取时间表
+        $url = 'http://hplus.com.vn/content/ajax_schedule/';
+        $snoopy = MySnnopy::init();
+        $snoopy->fetch($url);
+
+        if (!$snoopy->results) {
+             echo "没有数据返回";
+        }
+
+        $dates = [];
+        $crawler = new Crawler();
+        $crawler->addHtmlContent($snoopy->results, 'UTF-8');
+        $crawler->filter('option')->each(function(Crawler $node) use (&$dates) {
+            $dates[] = ['num' => $node->attr('value'), 'date' => date('Y-m-d', strtotime($node->text()))];
+        });
+
+
+        $schedule = [
+            'htv2' => ['id' => 'http://hplus.com.vn/xem-kenh-htv2-2630.html'],
+            'thao' => ['id' => 'http://hplus.com.vn/xem-kenh-htv-the-thao-hd-4009.html']
         ];
 
+        foreach ($schedule as $channelName => $value) {
+            foreach ($dates as $date) {
+
+                $snoopy = MySnnopy::init();
+                $form['id'] = $value['id'];
+                $form['num'] = $date['num'];
+                $snoopy->submit($url, $form);
+
+                if ($snoopy->results) {
+                    $todayFirst = $this->tomorrow;
+                    $data = $this->crawlTypeTwo($snoopy->results);
+                    $todayLast = $this->splitDay($data);
+
+                    if (!empty($todayFirst)) {
+                        $data = ArrayHelper::merge($todayFirst, $todayLast);
+                    } else {
+                        $data = $todayLast;
+                    }
+
+                    if ($data) {
+                        //查找频道是否存在
+                        $this->createParade($channelName, $date, $data, '122',  $value['id']);
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    public function splitDay($data)
+    {
+        $this->tomorrow = [];
+        $tomorrowFlag = false;
+
+        $today = [];
+        foreach ($data as $key => $value) {
+            if ($key > 0 && isset($data[$key-1])) {
+                $prev = strstr($data[$key-1]['parade_time'], ':', true);
+                $curr = strstr($value['parade_time'], ':', true);
+
+                if ($prev > $curr) {
+                    $tomorrowFlag = true;
+                }
+
+                if ($tomorrowFlag) {
+                    $this->tomorrow[] = $value;
+                } else {
+                    $today[] = $value;
+                }
+            }
+        }
+
+        return $today;
+    }
+
+    public function getHtvOne()
+    {
+        $data = [
+            'htv9' => ['url' => 'http://hplus.com.vn/xem-kenh-htv9-hd-2667.html', 'id' => '2667'],
+            'htv7' => ['url' => 'http://hplus.com.vn/xem-kenh-htv7-hd-256.html', 'id' => '2667'],
+            'htv3' => ['url' => 'http://hplus.com.vn/xem-kenh-htv3-2535.html', 'id' => '2535'],
+        ];
+
+        $snoopy = MySnnopy::init();
+
+        foreach ($data as $channelName => $value) {
+            $date = '2018-08-28';
+            $start = strtotime('yesterday');
+
+            for ($day = 0; $day <= 6; $day++) {
+                $form['contentId'] = $value['id'];
+                $form['date'] = date('Y-m-d', $start + 86400 * $day );
+                $snoopy->submit('http://hplus.com.vn/content/getcatchup_db/', $form);
+                $data = $this->crawlTypeOne($snoopy->results);
+                if ($data) {
+                    //查找频道是否存在
+                    $this->createParade($channelName, $date, $data, __CLASS__,  $value['url']);
+                }
+            }
+
+        }
+    }
+
+    private function crawlTypeOne($content)
+    {
+        $crawler = new Crawler();
+        $crawler->addHtmlContent($content, 'UTF-8');
+        $parade = [];
+        $crawler->filter('div.meta')->each(function(Crawler $node, $i) use (&$parade) {
+            try {
+                $time =  substr(trim(strstr($node->filter('span.time')->text(), '-', true)), 0, 5);
+                $content = $node->filter('h3.title')->text();
+                $parade[] = [
+                    'parade_time' => $time,
+                    'parade_name' => $content
+                ];
+                } catch (\Exception $e) {
+
+            }
+        });
+
+        return $parade;
+    }
+
+    private function crawlTypeTwo($content)
+    {
+        if ($content == false) {
+            return false;
+        }
+        $crawler = new Crawler();
+        $crawler->addHtmlContent($content, 'UTF-8');
+        $parade = [];
+        $crawler->filter('div.lps')->each(function(Crawler $node, $i) use (&$parade) {
+            try {
+                $time =  trim($node->filter('span')->text());
+                $content = trim(str_replace($time, '', $node->text()));
+                $parade[] = [
+                    'parade_time' => $time,
+                    'parade_name' => $content
+                ];
+            } catch (\Exception $e) {
+                echo $e->getMessage();
+            }
+        });
+
+        return $parade;
     }
 
     public function getTvSouData($channelCatagory)
