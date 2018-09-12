bd61c58b088c4a6e743a686a0040efe8bb2ca070
liziying <876505905@qq.com>
2018-09-12 13:27:12 +0800

    修改mac在线状态统计

--- console/jobs/SyncOnlineStateJob.php
+++ console/jobs/SyncOnlineStateJob.php
@@ -18,9 +18,10 @@ class SyncOnlineStateJob
     {
         $redis = MyRedis::init(MyRedis::REDIS_DEVICE_STATUS);
 
+        Mac::updateAll(['is_online' => 0], ['is_online' => 1]);
         foreach (Mac::find()->select('MAC')->asArray()->each() as $mac) {
-            $onLineState =  $redis->hmget($mac['MAC'], ['token', 'logintime']) ? true : false;
-            if ($onLineState && strtotime($onLineState['logintime']) > strtotime('today')) {
+            $onLineState =  $redis->hmget($mac['MAC'], ['token', 'logintime']);
+            if ($onLineState && time() - strtotime($onLineState['logintime']) <= 6 * 3600 ) {
                 Mac::updateAll(['is_online' => 1], ['MAC' => $mac['MAC']]);
             }
         }
