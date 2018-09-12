733c2457b5a30718fca27b2070aad4ba77a7102d
liziying <876505905@qq.com>
2018-09-12 11:41:50 +0800

    fix

--- console/jobs/SyncOnlineStateJob.php
+++ console/jobs/SyncOnlineStateJob.php
@@ -17,9 +17,7 @@ class SyncOnlineStateJob
     public static function start()
     {
         $redis = MyRedis::init(MyRedis::REDIS_DEVICE_STATUS);
-        $onLineState =  $redis->hget('D60FE09E9567', 'token') ? true : false;
-        Mac::updateAll(['is_online' => 1], ['MAC' => 'D60FE09E9567']);
-        var_dump($onLineState);exit;
+
         foreach (Mac::find()->select('MAC')->asArray()->each() as $mac) {
             $onLineState =  $redis->hget($mac['MAC'], 'token') ? true : false;
             if ($onLineState) {
