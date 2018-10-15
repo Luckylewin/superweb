29a1ead7c7997e2b97e8e9860753421902a8bf7f
liziying <876505905@qq.com>
2018-10-15 15:22:22 +0800

    修改

--- console/models/Movie.php
+++ console/models/Movie.php
@@ -28,53 +28,41 @@ class Movie extends Vod
         $title = trim($title);
         $vod = Vod::findOne(['vod_name' => $title]);
 
-        $db = Yii::$app->db;
-        $transaction = $db->beginTransaction();  //开启事务
-
-        try {
-            if (empty($vod)) {
-                $genre = VodList::findOne(['list_dir' => 'Movie']);
-                if (empty($genre)) {
-                    echo "请新增Movie分类" , PHP_EOL;
-                    return false;
-                }
-
-                $movie = new Vod();
-
-                $movie->vod_name = $title;
-                $movie->vod_pic = $image;
-                $movie->vod_pic_bg = $image;
-                $movie->vod_content = $info;
-                $movie->vod_area = $area;
-                $movie->vod_cid = $genre->list_id;
-                $movie->vod_trysee = 0;
-                $movie->save(false);
-
-                // 新增一个播放分组
-                $playGroup = new PlayGroup();
-                $playGroup->vod_id = $movie->vod_id;
-                $playGroup->group_name = $groupName;
-                $result = $playGroup->save(false);
-                var_dump($result);
-
-                // 新增播放链接
-                $link = new Vodlink();
-                $link->url = $url;
-                $link->episode = 1;
-                $link->group_id = $playGroup->id;
-                $link->save(false);
-
-                echo $title . "新增" . PHP_EOL;
-            } else {
-                echo $title . "存在" . PHP_EOL;
+        if (empty($vod)) {
+            $genre = VodList::findOne(['list_dir' => 'Movie']);
+            if (empty($genre)) {
+                echo "请新增Movie分类" , PHP_EOL;
+                return false;
             }
 
-        } catch (\Exception $e) {
-            $transaction->rollback();
-            echo $e->getMessage() . PHP_EOL;
-            sleep(5);
-        }
+            $movie = new Vod();
 
+            $movie->vod_name = $title;
+            $movie->vod_pic = $image;
+            $movie->vod_pic_bg = $image;
+            $movie->vod_content = $info;
+            $movie->vod_area = $area;
+            $movie->vod_cid = $genre->list_id;
+            $movie->vod_trysee = 0;
+            $movie->save(false);
+
+            // 新增一个播放分组
+            $playGroup = new PlayGroup();
+            $playGroup->vod_id = $movie->vod_id;
+            $playGroup->group_name = $groupName;
+            $playGroup->save(false);
+
+            // 新增播放链接
+            $link = new Vodlink();
+            $link->url = $url;
+            $link->episode = 1;
+            $link->group_id = $playGroup->id;
+            $link->save(false);
+            
+            echo $title . "新增" . PHP_EOL;
+        } else {
+            echo $title . "存在" . PHP_EOL;
+        }
         return false;
     }
 }
