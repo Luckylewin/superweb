dc9a6140cb51aa13ebc8ace337ecb357327df665
liziying <876505905@qq.com>
2018-10-31 15:44:57 +0800

    修改

--- console/controllers/CollectController.php
+++ console/controllers/CollectController.php
@@ -9,10 +9,13 @@
 namespace console\controllers;
 
 use console\collectors\youtube\Searcher;
+use console\models\Cartoon;
 use console\models\Karaoke;
 use console\models\Movie;
 use console\models\Tv;
+use console\models\Variety;
 use yii\console\Controller;
+use yii\helpers\Console;
 
 /**
  * 采集控制器
@@ -21,6 +24,17 @@ use yii\console\Controller;
  */
 class CollectController extends Controller
 {
+    public function actionHelp()
+    {
+        $this->stdout('php yii collect/karaoke [搜索内容] [地区] [语言] : 卡拉ok'.PHP_EOL, Console::FG_BLUE);
+        $this->stdout('php yii collect/tv [搜索内容] [地区] [语言] : 电视剧'.PHP_EOL, Console::FG_BLUE);
+        $this->stdout('php yii collect/movie [搜索内容] [地区] [语言] : 电影'.PHP_EOL, Console::FG_BLUE);
+        $this->stdout('php yii collect/cartoon [搜索内容] [地区] [语言] : 卡通'.PHP_EOL, Console::FG_BLUE);
+        $this->stdout('php yii collect/variety [搜索内容] [地区] [语言] : 综艺'.PHP_EOL, Console::FG_BLUE);
+
+        $this->stdout("示例: php yii collect/movie 电影 Chinese Chinese");
+    }
+
     // karaoke
     public function actionKaraoke($query = 'karaoke beat chuẩ', $area = 'Vietnam', $language = 'Vietnamese')
     {
@@ -61,6 +75,42 @@ class CollectController extends Controller
     public function actionMovie($query = "Vietnam movie", $area = 'Vietnam', $language = 'Vietnamese')
     {
         $movie  = new Movie();
+        $movie->setArea($area);
+        $movie->setLanguage($language);
+        $search = new Searcher($movie);
+        $options = [
+            'q' => $query,//
+            'maxResults' => 50,
+            'type' =>  'video',
+            'order' => 'relevance',
+            'videoDuration' => 'long',
+        ];
+        $search->setQueryOption($options);
+        $search->start();
+    }
+
+    // 采集Youtube 动漫
+    public function actionCartoon($query = "cartoons for kids", $area = 'Vietnam', $language = 'Vietnamese')
+    {
+        $movie  = new Cartoon();
+        $movie->setArea($area);
+        $movie->setLanguage($language);
+        $search = new Searcher($movie);
+        $options = [
+            'q' => $query,//
+            'maxResults' => 50,
+            'type' =>  'video',
+            'order' => 'relevance',
+            'videoDuration' => 'long',
+        ];
+        $search->setQueryOption($options);
+        $search->start();
+    }
+
+    // 采集Youtube 动漫
+    public function actionVariety($query = "vietnam variety", $area = 'Vietnam', $language = 'Vietnamese')
+    {
+        $movie  = new Variety();
         $movie->setArea($area);
         $movie->setLanguage($language);
         $search = new Searcher($movie);
---
+++ console/models/Cartoon.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lychee
+ * Date: 2018/10/15
+ * Time: 13:43
+ */
+
+namespace console\models;
+
+use backend\models\PlayGroup;
+use common\models\Vod;
+use common\models\Vodlink;
+use common\models\VodList;
+use Yii;
+
+class Cartoon extends Vod
+{
+    public $language;
+    public $area;
+
+    public function setLanguage($language = 'Vietnamese')
+    {
+        $this->language = $language;
+    }
+
+    public function setArea($area = 'Vietnam')
+    {
+        $this->area = $area;
+    }
+
+    public function collect($data, $playGroupName = 'default')
+    {
+        $title = $data['title'];
+        $url   = $data['url'];
+        $image = $data['image'];
+        $info  = $data['info'];
+        $groupName = $playGroupName;
+
+        $title = trim($title);
+        $vod = Vod::findOne(['vod_name' => $title]);
+
+        if (empty($vod)) {
+            $genre = VodList::findOne(['list_dir' => 'Cartoon']);
+            if (empty($genre)) {
+                echo "请新增Cartoon分类" , PHP_EOL;
+                return false;
+            }
+
+            $movie = new Vod();
+
+            $movie->vod_name = $title;
+            $movie->vod_pic = "http://192.200.112.162:12389/proxy?path=".$image;
+            $movie->vod_pic_bg = $movie->vod_pic;
+            $movie->vod_content = $info;
+            $movie->vod_area = $this->area;
+            $movie->vod_language = $this->language;
+            $movie->vod_cid = $genre->list_id;
+            $movie->vod_trysee = 0;
+            $movie->save(false);
+
+            // 新增一个播放分组
+            $playGroup = new PlayGroup();
+            $playGroup->vod_id = $movie->vod_id;
+            $playGroup->group_name = $groupName;
+            $playGroup->save(false);
+
+            // 新增播放链接
+            $link = new Vodlink();
+            $link->url = $url;
+            $link->episode = 1;
+            $link->group_id = $playGroup->id;
+            $link->save(false);
+
+            echo $title . "新增" . PHP_EOL;
+        } else {
+            echo $title . "存在" . PHP_EOL;
+        }
+        return false;
+    }
+}
